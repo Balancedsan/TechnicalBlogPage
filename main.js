7fa555d4582b020036c025ea19534769fe7bd7ab
John Wee <johnwee35@gmail.com>
2018-09-01 02:19:27 +0800

    final

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ toggleBtn.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("active");
 })
 
+
+// document.querySelector("#nav1").addEventListener("click",function(){
+//     document.querySelector('#introduction').scrollIntoView({ behavior: 'smooth' });
+// });
+
+// document.querySelector("#nav2").addEventListener("click",function(){
+//     document.querySelector('#domNode').scrollIntoView({ behavior: 'smooth' });
+// });
+
+// document.querySelector("#nav3").addEventListener("click",function(){
+//     document.querySelector('#traverse').scrollIntoView({ behavior: 'smooth' });
+// });
+
+// document.querySelector("#nav4").addEventListener("click",function(){
+//     document.querySelector('#event').scrollIntoView({ behavior: 'smooth' });
+// });
+
+// document.querySelector("#nav5").addEventListener("click",function(){
+//     document.querySelector('#summary').scrollIntoView({ behavior: 'smooth' });
+// });
+
+
+
+
+
